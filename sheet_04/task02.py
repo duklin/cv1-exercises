f74c04a03c7e09f114f8a2e2cc616cc71d9fe25a
Dushko Klincharov <klincharovdushko@gmail.com>
2021-11-25 23:56:49 +0100

    [Exercise 04] Task 2 solution by Dushko

--- sheet_04/task02.py
+++ sheet_04/task02.py
@@ -1,17 +1,15 @@
 import numpy as np
 import cv2
 import matplotlib.pyplot as plt
-from matplotlib import rc
-rc('text', usetex=True)  # if you do not have latex installed simply uncomment this line + line 75
 
 
 def load_data():
-    """ loads the data for this task
+    """loads the data for this task
     :return:
     """
-    fpath = 'images/ball.png'
+    fpath = "images/ball.png"
     radius = 70
-    Im = cv2.imread(fpath, 0).astype('float32')/255  # 0 .. 1
+    Im = cv2.imread(fpath, 0).astype("float32") / 255  # 0 .. 1
 
     # we resize the image to speed-up the level set method
     Im = cv2.resize(Im, dsize=(0, 0), fx=0.5, fy=0.5)
@@ -26,7 +24,7 @@ def load_data():
 
 
 def get_contour(phi):
-    """ get all points on the contour
+    """get all points on the contour
     :param phi:
     :return: [(x, y), (x, y), ....]  points on contour
     """
@@ -38,6 +36,7 @@ def get_contour(phi):
     Y, X = np.nonzero(D)
     return np.array([X, Y]).transpose()
 
+
 # ===========================================
 # RUNNING
 # ===========================================
@@ -46,10 +45,46 @@ def get_contour(phi):
 # ------------------------
 # your implementation here
 
-# ------------------------
+
+def get_phi_derivatives(phi: np.ndarray) -> dict:
+    """Returns first and second derivatives of the level-set function
+    approximated with central difference"""
+    phi = np.pad(phi, pad_width=1)
+    phi_x = 0.5 * (phi[1:-1, 2:] - phi[1:-1, :-2])
+    phi_y = 0.5 * (phi[2:, 1:-1] - phi[:-2, 1:-1])
+    phi_xx = phi[1:-1, 2:] - 2 * phi[1:-1, 1:-1] + phi[1:-1, :-2]
+    phi_yy = phi[2:, 1:-1] - 2 * phi[1:-1, 1:-1] + phi[:-2, 1:-1]
+    phi_xy = 0.25 * (phi[2:, 2:] - phi[:-2, 2:] - phi[2:, :-2] + phi[:-2, :-2])
+    return {"x": phi_x, "y": phi_y, "xx": phi_xx, "yy": phi_yy, "xy": phi_xy}
 
 
-if __name__ == '__main__':
+def get_phi_onesided_derivatives(phi: np.ndarray) -> dict:
+    """Returns first derivatives of the level-set function
+    approximated with one-sided difference"""
+    phi = np.pad(phi, pad_width=1)
+    right = phi[1:-1, 2:] - phi[1:-1, 1:-1]
+    left = phi[1:-1, 1:-1] - phi[1:-1, :-2]
+    down = phi[2:, 1:-1] - phi[1:-1, 1:-1]
+    up = phi[1:-1, 1:-1] - phi[:-2, 1:-1]
+    return {"right": right, "left": left, "up": up, "down": down}
+
+
+def get_gradient(img: np.ndarray) -> np.ndarray:
+    sobelx = cv2.Sobel(img, cv2.CV_64F, 1, 0, ksize=3)
+    sobely = cv2.Sobel(img, cv2.CV_64F, 0, 1, ksize=3)
+    grad = np.sqrt(np.square(sobelx) + np.square(sobely))
+    return grad
+
+
+def get_weight_derivatives(w: np.ndarray) -> dict:
+    """Returns first derivatives of the weighting function"""
+    w = np.pad(w, pad_width=1)
+    w_x = 0.5 * (w[1:-1, 2:] - w[1:-1, :-2])
+    w_y = 0.5 * (w[2:, 1:-1] - w[:-2, 1:-1])
+    return {"x": w_x, "y": w_y}
+
+
+if __name__ == "__main__":
 
     n_steps = 20000
     plot_every_n_step = 100
@@ -62,28 +97,52 @@ if __name__ == '__main__':
 
     # ------------------------
     # your implementation here
+    gradient = get_gradient(Im)
+    w = 1 / (gradient + 1)
+    dw = get_weight_derivatives(w)
 
+    tau = 1 / (5 * np.max(w))
+    epsilon = 1e-4
     # ------------------------
 
     for t in range(n_steps):
 
         # ------------------------
         # your implementation here
-
+        d_phi = get_phi_derivatives(phi)
+        mean_curv_term = (
+            tau
+            * w
+            * (
+                d_phi["xx"] * d_phi["y"] * d_phi["y"]
+                - 2 * d_phi["x"] * d_phi["y"] * d_phi["xy"]
+                + d_phi["yy"] * d_phi["x"] * d_phi["x"]
+            )
+            / (d_phi["x"] * d_phi["x"] + d_phi["y"] * d_phi["y"] + epsilon)
+        )
+        onesided_d_phi = get_phi_onesided_derivatives(phi)
+        towards_edges_term = (
+            np.max(dw["x"], 0) * onesided_d_phi["right"]
+            + np.min(dw["x"], 0) * onesided_d_phi["left"]
+            + np.max(dw["y"], 0) * onesided_d_phi["down"]
+            + np.min(dw["y"], 0) * onesided_d_phi["up"]
+        )
+        phi = phi + mean_curv_term
+        # phi = phi + mean_curv_term + towards_edges_term
         # ------------------------
 
         if t % plot_every_n_step == 0:
             ax1.clear()
-            ax1.imshow(Im, cmap='gray')
-            ax1.set_title('frame ' + str(t))
+            ax1.imshow(Im, cmap="gray")
+            ax1.set_title("frame " + str(t))
 
             contour = get_contour(phi)
             if len(contour) > 0:
-                ax1.scatter(contour[:, 0], contour[:, 1], color='red', s=1)
+                ax1.scatter(contour[:, 0], contour[:, 1], color="red", s=1)
 
             ax2.clear()
             ax2.imshow(phi)
-            ax2.set_title(r'$\phi$', fontsize=22)
+            ax2.set_title(r"$\phi$", fontsize=22)
             plt.pause(0.01)
 
     plt.show()
