e4e10f4550e4486e1514765c9bef83d2142a626e
Dushko Klincharov <klincharovdushko@gmail.com>
2021-11-25 20:29:17 +0100

    [Exercise 04] Initial files

---
+++ sheet_04/task01.py
@@ -0,0 +1,90 @@
+import matplotlib.pyplot as plt
+import numpy.linalg as la
+import numpy as np
+import cv2
+
+
+def plot_snake(ax, V, fill='green', line='red', alpha=1, with_txt=False):
+    """ plots the snake onto a sub-plot
+    :param ax: subplot (fig.add_subplot(abc))
+    :param V: point locations ( [ (x0, y0), (x1, y1), ... (xn, yn)]
+    :param fill: point color
+    :param line: line color
+    :param alpha: [0 .. 1]
+    :param with_txt: if True plot numbers as well
+    :return:
+    """
+    V_plt = np.append(V.reshape(-1), V[0,:]).reshape((-1, 2))
+    ax.plot(V_plt[:,0], V_plt[:,1], color=line, alpha=alpha)
+    ax.scatter(V[:,0], V[:,1], color=fill,
+               edgecolors='black',
+               linewidth=2, s=50, alpha=alpha)
+    if with_txt:
+        for i, (x, y) in enumerate(V):
+            ax.text(x, y, str(i))
+
+
+def load_data(fpath, radius):
+    """
+    :param fpath:
+    :param radius:
+    :return:
+    """
+    Im = cv2.imread(fpath, 0)
+    h, w = Im.shape
+    n = 20  # number of points
+    u = lambda i: radius * np.cos(i) + w / 2
+    v = lambda i: radius * np.sin(i) + h / 2
+    V = np.array(
+        [(u(i), v(i)) for i in np.linspace(0, 2 * np.pi, n + 1)][0:-1],
+        'int32')
+
+    return Im, V
+
+
+# ===========================================
+# RUNNING
+# ===========================================
+
+# FUNCTIONS
+# ------------------------
+# your implementation here
+
+# ------------------------
+
+
+def run(fpath, radius):
+    """ run experiment
+    :param fpath:
+    :param radius:
+    :return:
+    """
+    Im, V = load_data(fpath, radius)
+
+    fig = plt.figure(figsize=(10, 10))
+    ax = fig.add_subplot(111)
+    n_steps = 200
+
+    # ------------------------
+    # your implementation here
+
+    # ------------------------
+
+    for t in range(n_steps):
+        # ------------------------
+        # your implementation here
+
+        # ------------------------
+
+        ax.clear()
+        ax.imshow(Im, cmap='gray')
+        ax.set_title('frame ' + str(t))
+        plot_snake(ax, V)
+        plt.pause(0.01)
+
+    plt.pause(2)
+
+
+if __name__ == '__main__':
+    run('images/ball.png', radius=120)
+    run('images/coffee.png', radius=100)
---
+++ sheet_04/task02.py
@@ -0,0 +1,89 @@
+import numpy as np
+import cv2
+import matplotlib.pyplot as plt
+from matplotlib import rc
+rc('text', usetex=True)  # if you do not have latex installed simply uncomment this line + line 75
+
+
+def load_data():
+    """ loads the data for this task
+    :return:
+    """
+    fpath = 'images/ball.png'
+    radius = 70
+    Im = cv2.imread(fpath, 0).astype('float32')/255  # 0 .. 1
+
+    # we resize the image to speed-up the level set method
+    Im = cv2.resize(Im, dsize=(0, 0), fx=0.5, fy=0.5)
+
+    height, width = Im.shape
+
+    centre = (width // 2, height // 2)
+    Y, X = np.ogrid[:height, :width]
+    phi = radius - np.sqrt((X - centre[0]) ** 2 + (Y - centre[1]) ** 2)
+
+    return Im, phi
+
+
+def get_contour(phi):
+    """ get all points on the contour
+    :param phi:
+    :return: [(x, y), (x, y), ....]  points on contour
+    """
+    eps = 1
+    A = (phi > -eps) * 1
+    B = (phi < eps) * 1
+    D = (A - B).astype(np.int32)
+    D = (D == 0) * 1
+    Y, X = np.nonzero(D)
+    return np.array([X, Y]).transpose()
+
+# ===========================================
+# RUNNING
+# ===========================================
+
+# FUNCTIONS
+# ------------------------
+# your implementation here
+
+# ------------------------
+
+
+if __name__ == '__main__':
+
+    n_steps = 20000
+    plot_every_n_step = 100
+
+    Im, phi = load_data()
+
+    fig = plt.figure(figsize=(16, 8))
+    ax1 = fig.add_subplot(121)
+    ax2 = fig.add_subplot(122)
+
+    # ------------------------
+    # your implementation here
+
+    # ------------------------
+
+    for t in range(n_steps):
+
+        # ------------------------
+        # your implementation here
+
+        # ------------------------
+
+        if t % plot_every_n_step == 0:
+            ax1.clear()
+            ax1.imshow(Im, cmap='gray')
+            ax1.set_title('frame ' + str(t))
+
+            contour = get_contour(phi)
+            if len(contour) > 0:
+                ax1.scatter(contour[:, 0], contour[:, 1], color='red', s=1)
+
+            ax2.clear()
+            ax2.imshow(phi)
+            ax2.set_title(r'$\phi$', fontsize=22)
+            plt.pause(0.01)
+
+    plt.show()
