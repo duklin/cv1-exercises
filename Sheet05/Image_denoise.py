ef35f3409d2894b49154de69dc7584b3e3d56616
Saurabh Gupta <saurabh.gupta1002@gmail.com>
2021-12-02 20:12:52 +0100

    Complete task 4 sheet 5

--- Sheet05/Image_denoise.py
+++ Sheet05/Image_denoise.py
@@ -2,7 +2,6 @@ import cv2
 import numpy as np
 import maxflow
 
-
 def question_3(I, rho=0.7, pairwise_cost_same=0.005, pairwise_cost_diff=0.2):
 
     h, w = I.shape
@@ -15,25 +14,27 @@ def question_3(I, rho=0.7, pairwise_cost_same=0.005, pairwise_cost_diff=0.2):
 
     ### 3) Compute Unary cost
 
+    ## Edges to source
     U0_pairwise = np.zeros_like(I)
-    U0_pairwise[1:-1, 1:-1] = 2 * pairwise_cost_same
-    U0_pairwise[0, 1:] = pairwise_cost_same
-    U0_pairwise[1:, 0] = pairwise_cost_same
-    U0_pairwise[1:, -1] = 2 * pairwise_cost_same
-    U0_pairwise[-1, 1:-1] = 2 * pairwise_cost_same
+    U0_pairwise[1:-1, 1:-1] = 2 * pairwise_cost_same    # 4 neighborhood
+    U0_pairwise[0, 1:] = pairwise_cost_same             # 2/3 neighborhood
+    U0_pairwise[1:, 0] = pairwise_cost_same             # 2/3 neighborhood
+    U0_pairwise[1:, -1] = 2 * pairwise_cost_same        # 2/3 neighborhood
+    U0_pairwise[-1, 1:-1] = 2 * pairwise_cost_same      # 2/3 neighborhood
 
+    ## Edges to sink
     U1_pairwise = np.zeros_like(I)
-    U1_pairwise[1:-1, 1:-1] = 2 * pairwise_cost_same
-    U1_pairwise[0, 0] = 2 * pairwise_cost_same
-    U1_pairwise[0, 1:-1] = 2 * pairwise_cost_same
-    U1_pairwise[0, -1] = pairwise_cost_same
-    U1_pairwise[1:-1, 0] = 2 * pairwise_cost_same
-    U1_pairwise[-1, 0] = pairwise_cost_same
-    U1_pairwise[1:-1, -1] = pairwise_cost_same
-    U1_pairwise[-1, 1:-1] = pairwise_cost_same
+    U1_pairwise[1:-1, 1:-1] = 2 * pairwise_cost_same    # 4 neighborhood
+    U1_pairwise[0, 1:-1] = 2 * pairwise_cost_same       # 3 neighborhood
+    U1_pairwise[1:-1, 0] = 2 * pairwise_cost_same       # 3 neighborhood
+    U1_pairwise[1:-1, -1] = pairwise_cost_same          # 3 neighborhood
+    U1_pairwise[-1, 1:-1] = pairwise_cost_same          # 3 neighborhood
+    U1_pairwise[0, 0] = 2 * pairwise_cost_same          # 2 neighborhood
+    U1_pairwise[0, -1] = pairwise_cost_same             # 2 neighborhood
+    U1_pairwise[-1, 0] = pairwise_cost_same             # 2 neighborhood
     
-    U0 = np.where(I == 0, 1 - rho, rho) + U0_pairwise
-    U1 = np.where(I == 0, rho, 1 - rho) + U1_pairwise
+    U0 = -np.log(np.where(I == 0, 1 - rho, rho)) + U0_pairwise
+    U1 = -np.log(np.where(I == 255, 1 - rho, rho)) + U1_pairwise
 
     ### 4) Add terminal edges
     g.add_grid_tedges(grid_nodes, U0, U1)
@@ -49,41 +50,85 @@ def question_3(I, rho=0.7, pairwise_cost_same=0.005, pairwise_cost_diff=0.2):
     structure_ver = structure_hor.T
     g.add_grid_edges(grid_nodes, weights=1, structure=structure_ver)
     
-    # (Keep in mind the stucture of neighbourhood and set the weights according to the pairwise potential)
-
     ### 6) Maxflow
     g.maxflow()
     
-    Denoised_I = np.where(g.get_grid_segments(grid_nodes), 0, 255)
+    Denoised_I = np.where(g.get_grid_segments(grid_nodes), 255, 0)
     Denoised_I = np.array(Denoised_I, np.uint8)
     # Do not use the close button on image window to close, instead press enter (or any other key) to close windows. 
     cv2.imshow('Original Img', I)
     cv2.imshow('Denoised Img', Denoised_I), cv2.waitKey(0), cv2.destroyAllWindows()
 
 
-def question_4(I, rho=0.6):
+def potts(wm, wn):
+    if wm == wn:
+        return 0
+    return 0.35
 
+def add_single_edge(graph, node, neighbor, node_label, neighbor_label, current_label):
+    if node_label == neighbor_label == current_label:
+        return
+    if node_label == current_label:
+        graph.add_edge(node, neighbor, 0, potts(node_label, neighbor_label))
+        return
+    if (node_label == neighbor_label) and (node_label != current_label):
+        graph.add_edge(node, neighbor, potts(node_label, current_label), potts(current_label, node_label))
+        return
+    if (node_label != current_label) and (neighbor_label != current_label) and (node_label != neighbor_label):
+        new_node_id = graph.add_nodes(1)
+        graph.add_edge(node, new_node_id, potts(node_label, current_label), np.inf)
+        graph.add_edge(new_node_id, neighbor, np.inf, potts(current_label, neighbor_label))
+        graph.add_tedge(new_node_id, 0, potts(node_label, neighbor_label))
+        return
+
+def question_4(I, rho=0.6):
     labels = np.unique(I).tolist()
 
-    Denoised_I = np.zeros_like(I)
+    Denoised_I = np.copy(I)
     ### Use Alpha expansion binary image for each label
 
-    ### 1) Define Graph
+    h, w = I.shape
+    num_pixels = h * w
 
-    ### 2) Add pixels as nodes
+    Denoise_old = np.copy(I)
+    while(True):
+        for label in labels:
+            ### 1) Define Graph
+            g = maxflow.Graph[float](num_pixels, num_pixels)
 
-    ### 3) Compute Unary cost
+            ### 2) Add pixels as nodes
+            nodeid = g.add_grid_nodes(Denoised_I.shape)
 
-    ### 4) Add terminal edges
+            ### 3) Compute Unary costget_grid_segments
+            U_source = -np.log(np.where(Denoised_I == label, rho, 0.5 * (1 - rho)))
+            U_sink = np.where(Denoised_I == label, np.inf, -np.log(rho))
 
-    ### 5) Add Node edges
-    ### Vertical Edges
+            ### 4) Add terminal edges
+            g.add_grid_tedges(nodeid, U_source, U_sink)
 
-    ### Horizontal edges
-    # (Keep in mind the stucture of neighbourhood and set the weights according to the pairwise potential)
+            ## 5) Add Node edges
+            for i in range(0, h-1):
+                for j in range(0, w-1):
+                    add_single_edge(g, nodeid[i, j], nodeid[i+1, j], Denoised_I[i, j], Denoised_I[i+1, j], label)
+                    add_single_edge(g, nodeid[i, j], nodeid[i, j+1], Denoised_I[i, j], Denoised_I[i, j+1], label)
+            for i in range(0, h-1):
+                add_single_edge(g, nodeid[i, -1], nodeid[i+1, -1], Denoised_I[i, -1], Denoised_I[i+1, -1], label)
 
-    ### 6) Maxflow
+            for j in range(0, w-1):
+                add_single_edge(g, nodeid[-1, j], nodeid[-1, j+1], Denoised_I[-1, j], Denoised_I[-1, j+1], label)
 
+            # (Keep in mind the stucture of neighbourhood and set the weights according to the pairwise potential)
+
+            ### 6) Maxflow
+            g.maxflow()
+
+            seg = g.get_grid_segments(nodeid)
+
+            Denoised_I[seg] = label
+        
+        if(Denoise_old.all() == Denoised_I.all()):
+            break
+        Denoise_old = Denoised_I
 
     # Do not use the close button on image window to close, instead press enter (or any other key) to close windows. 
     cv2.imshow('Original Img', I), \
@@ -100,10 +145,7 @@ def main():
     question_3(image_q3, rho=0.7, pairwise_cost_same=0.005, pairwise_cost_diff=0.55)
 
     ### Call solution for question 4, maybe test it 0 < rho <= 0.2 to see which one works better
-    # question_4(image_q4, rho=0.2)
+    question_4(image_q4, rho=0.37)
 
 if __name__ == "__main__":
     main()
-
-
-
